b1bde8742aef0eb9834dac84b9b5ba5a1de13bb4
Anton Kononchenko <akononche@gmail.com>
2024-06-16 03:00:46 +0300

    Aaaa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const tg = window.Telegram.WebApp;
+
+const images = [
+    // Добавьте URL-адреса или импортируйте изображения здесь
+    'logo192.png',
+    'logo512.png',
+    // ...
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [image, setImage] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [isCalculating, setIsCalculating] = useState(false);
+
+    useEffect(() => {
+        tg.ready()
+    }, []);
+
+    const onClose = () => {
+        tg.close()
+    }
+    const getResult = () => {
+        setImage(null); // Убираем предыдущую картинку
+        setIsCalculating(true);
+        setLoading(true);
+        setTimeout(() => {
+            const randomImage = images[Math.floor(Math.random() * images.length)];
+            setImage(randomImage);
+            setLoading(false);
+            setIsCalculating(false);
+        }, 3000);
+    };
+
+    return (
+        <div className="App">
+            {!image && !isCalculating && (
+                <>
+                    <h1>Нажми сюда</h1>
+                    <div className="arrow-down">↓</div>
+                    <button onClick={getResult}>Рассчитать результат</button>
+                </>
+            )}
+            {loading && <div className="loading">Calculating...</div>}
+            {image && !loading && (
+                <div className="result">
+                    <img src={image} alt="Result" />
+                    <button onClick={getResult}>Получить прогноз</button>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
