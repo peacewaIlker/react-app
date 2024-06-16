36c931b297b21e6b39612c640055707074dcb672
Anton Kononchenko <akononche@gmail.com>
2024-06-16 22:13:57 +0300

    НадеÐюсь рабочая картинка

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 import './App.css';
 
 const images = [
-    'image1.jpg',
+    'image1.jpg', // Убедитесь, что пути к изображениям корректны
     'logo192.png',
     'logo512.png'
-    // Убедитесь, что пути к изображениям корректны
     // ...
 ];
 
@@ -28,22 +27,25 @@ function App() {
 
     return (
         <div className="App">
-            {isCalculating && <div className="loading">Calculating...</div>}
-            {!isCalculating && !image && (
-                <>
-                    <h1>Нажми сюда</h1>
-                    <div className="arrow-down">↓</div>
-                    <button onClick={getResult}>Рассчитать результат</button>
-                </>
-            )}
-            {!isCalculating && image && (
-                <div className="result-container">
-                    <div className="image-container">
+            <div className="top-container">
+                {isCalculating && <div className="loading">Загрузка...</div>}
+                {!isCalculating && !image && (
+                    <>
+                        <h1>Нажми сюда</h1>
+                        <div className="arrow-down">↓</div>
+                    </>
+                )}
+            </div>
+            <div className="result-container">
+                <div className="image-container">
+                    {image && !loading && (
                         <img src={image} alt="Result" />
-                    </div>
-                    <button onClick={getResult}>Получить прогноз</button>
+                    )}
                 </div>
-            )}
+                <button onClick={getResult}>
+                    {isCalculating || !image ? 'Рассчитать результат' : 'Получить прогноз'}
+                </button>
+            </div>
         </div>
     );
 }
